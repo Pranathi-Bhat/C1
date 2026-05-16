58e864a879559d116bd274a62768f8f1ef79fb14
Pranathi-Bhat <pranathibhat23@gmail.com>
2026-05-16 08:56:50 +0530

    Added 9.c

---
+++ 9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,hcf,lcm,temp1,temp2,remainder;
+    printf("Enter two numbers:");
+    scanf("%d %d",&num1,&num2);
+    
+    temp1=num1;
+    temp2=num2;
+
+    while (temp2!=0){
+        remainder=temp1%temp2;
+        temp1=temp2;
+        temp2=remainder;
+    }
+    hcf=temp1;
+    lcm=(num1*num2)/hcf;
+
+    printf("HCF=%d\n",hcf);
+    printf("LCM=%d\n",lcm);
+
+    return 0;
+}
